cde2dc7a635a899df2447a7c9de8544e38ba0e29
cag2050 <cag2050@163.com>
2017-11-25 16:02:20 +0800

    m

---
+++ src/main/java/com/cag/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.cag.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(value = {"/", "/hello"}, method = {RequestMethod.GET, RequestMethod.POST}, produces = {"application/json; charset=UTF-8"})
+    public String helloWorld() {
+        return "{\'word\':\'hello world\'}";
+    }
+}
